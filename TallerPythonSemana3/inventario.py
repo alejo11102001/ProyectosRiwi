2fea320c7f65fa0035eda035e6a30b43d3207ccc
Diego Zuluaga <diego.zuluagaye@amigo.edu.co>
2025-05-06 21:26:32 -0500

    taller 3

---
+++ TallerPythonSemana3/inventario.py
@@ -0,0 +1,17 @@
+#añadir producto
+## nombre, precio, cantidad disponible
+## diccionario
+
+#consultar productos
+##buscar producto por nombre => detalle de precio y cantidad
+##Si el producto no está en el inventario, se debe notificar adecuadamente
+
+#actualizar precios
+## actualizar mediante el nombre del producto
+
+# Eliminar productos:
+## El programa debe permitir al usuario eliminar productos del inventario de manera segura
+
+# Calcular el valor total del inventario:
+## El programa debe calcular el valor total de los productos en inventario y mostrarlo al usuario
+## Para ello, utilizarás una función anónima (lambda) que facilite este cálculo.
